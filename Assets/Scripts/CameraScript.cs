c0a84941b6da5af0b91786d89e45c1d510e0f7a3
Rutger van Schoonderwalt <Rutger.van.Schoonderwalt@hva.nl>
2017-12-21 10:18:55 +0100

    prototype

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public GameObject santa;
+    public float camSpeed;
+    Vector3 camPos;
+
+	void Start ()
+    {
+        camPos = transform.position;
+    }
+	
+	void Update ()
+    {
+        MoveCamera();
+	}
+
+    void MoveCamera()
+    {
+        transform.Translate(Vector2.down * camSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/SantaScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SantaScript : MonoBehaviour {
+
+    public float speed;
+    public float rotateSpeed;
+
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        Move();
+	}
+
+    void Move()
+    {
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
+
+        transform.Rotate(0, 0, rotateSpeed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rotateSpeed *= -1;
+        }
+    }
+}
