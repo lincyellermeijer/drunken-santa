76c64d871edcf30e03aabd51c5de8724e1555ef9
Lisa The <lisathe97@gmail.com>
2017-12-21 12:20:22 +0100

    [Add] Obstacles - Includes object pool and spawn manager

---
+++ Assets/Scripts/Obstacles.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour {
+
+    private Camera cam;
+    private Transform deletePoint;
+
+    private void Awake()
+    {
+        cam = Camera.main;
+        deletePoint = cam.transform.GetChild(2);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       DisableGameObject();
+    }
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("you lose");
+        }
+    }
+
+    public void DisableGameObject()
+    {
+        if (transform.position.y > deletePoint.transform.position.y)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+
+    //Object Poolers
+
+    public ObjectPool obstaclePool;
+
+    // Spawn Range
+
+    public Transform minSpawnPos, maxSpawnPos;
+    private Vector3 spawnPosition;
+
+    //Timers
+    public float obstacleSpawnInterval = 5f;
+    float obstacleTimer = 0;
+    
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (obstacleTimer < obstacleSpawnInterval)
+        {
+            obstacleTimer += Time.deltaTime;
+        }
+        else
+        {
+            obstacleTimer = 0;
+            SpawnObject(obstaclePool);
+           
+        }
+
+    }
+
+    //Retrieves object from the pool and spawns it on a random y location
+    void SpawnObject(ObjectPool pool)
+    {
+        spawnPosition = new Vector3( Random.Range(minSpawnPos.position.x, maxSpawnPos.position.x), minSpawnPos.position.y, 1);
+        GameObject newItem = pool.GetPooledObject();
+        newItem.transform.position = spawnPosition;
+        newItem.transform.rotation = transform.rotation;
+        newItem.SetActive(true);
+    }
+}
