4b658ec2e1bc2277955ffe2435b4b9699f6417cf
Lisa The <lisathe97@gmail.com>
2017-12-22 10:07:09 +0100

    [Add] - Scrolling background - Borders on the side - Camera prefab [Edit] - Santa (Added Player tag, rb to dynamic with gravity 0 )

---
+++ Assets/Scripts/Border.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Border : MonoBehaviour {
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("you lose");
+        }
+    }
+}
